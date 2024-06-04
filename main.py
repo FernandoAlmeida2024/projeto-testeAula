e09bce29f896c01c795ade8f69293ee00668a65a
Fernando Almeida <fernandora505@hotmail.com>
2024-06-04 18:14:04 -0300

    Desafio do Curso Mestres da Automação

---
+++ main.py
@@ -0,0 +1,6 @@
+import time #Importar modulo time
+for i in range(1,10): #Criando laço de repetição
+    print("Estou Aprendendo a Automatizar Projetos")
+    time.sleep(1)
+print("Automação Concluída") #Imprimindo Mensagem ao fim do laço
+(print("Curso Dev Aprender"))
